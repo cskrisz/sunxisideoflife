d4848dfe0410d6d82e95fcf87eef33a05e7aa3e1
cskrisz <cskrisztianster@gmail.com>
2016-07-12 14:30:59 +0200

    Create before.sh

---
+++ before.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+  
+if [ ! -f buildroot-2016.05.tar.gz ]; then
+    wget https://buildroot.org/downloads/buildroot-2016.05.tar.gz --no-check-certificate
+    tar xf buildroot-2016.05.tar.gz --keep-old-files
+fi
+
+if [ ! -f sunxi-3.4.zip ]; then
+    wget https://github.com/linux-sunxi/linux-sunxi/archive/sunxi-3.4.zip --no-check-certificate
+    tar xf sunxi-3.4.zip
+fi
